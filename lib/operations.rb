b0b51dd83ef4b1d554a7d56c485ea27cd09a7738
Tara Ellicott <taraellicott@gmail.com>
2018-04-11 19:09:11 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,11 @@
 def unsafe?(speed)
-40 < speed < 60 ? false : true
+speed > 40 && speed < 60 ? false : true
 end
 
 
 
 def not_safe?(speed)
-40 < speed < 60 ? false : true	
+speed > 40 && speed < 60 ? false : true	
 end
 	
 
